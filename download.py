eea05e98a4a2a1e457e4f55c549080fe3e1ced4c
Jônatas Renan <jonatas@dcc.ufmg.br>
2018-02-27 19:21:43 -0300

    Download dos trabalhos

--- download.py
+++ download.py
@@ -3,3 +3,24 @@
 
     jonatasrenan@dcc.ufmg.br
 """
+
+def download(trabalho):
+    """
+    Baixa o trabalho na subpasta trabalhos
+    :param trabalho: dicionario contendo os dados do trabalho
+    :return:
+    """
+    from requests.utils import requote_uri
+    import os, urllib
+
+    url = trabalho['trabalho_link']
+    name = url.rsplit('/', 1)[-1]
+    filename = os.path.join('./trabalhos', name)
+    url = requote_uri(url)
+    if not url:
+        exit
+    if not os.path.isfile(filename):
+        try:
+            urllib.request.urlretrieve(url, filename)
+        except:
+            print("Erro: %s\n" % url)
