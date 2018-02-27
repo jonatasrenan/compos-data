1d1959fee75ef5cea74735003fad2583a0c0d3e4
Jônatas Renan <jonatas@dcc.ufmg.br>
2018-02-27 19:16:55 -0300

    Funções base: map e acesso

--- dados.py
+++ dados.py
@@ -5,3 +5,38 @@
 """
 
 from raspagem import raspar_encontro, raspar_gt, raspar_trabalho
+
+
+def map(fn, l):
+    """
+    Redefinição da função map, paralela, aguarda threads no final e retorna resultado expandido em lista.
+    :param fn: função
+    :param l: lista
+    :return: resultado do fn em l expandido em lista
+    """
+    import concurrent.futures
+    threads = concurrent.futures.ThreadPoolExecutor(max_workers=20)
+    result = threads.map(fn, l)
+    threads.shutdown(wait=True)
+    return list(result)
+
+
+def acessar(endereco, dados, seletor):
+    """
+    Acessa o "endereço" utilizando o método POST e enviando "dados";
+    filtra resposta utilizando um determinado "seletor" CSS
+    :param endereco: endereço a ser acessado
+    :param dados: campos enviados na requisição post
+    :param seletor: seletor aplicado na resposta da requisição
+    :return: resultados da seleção
+    """
+    import requests
+    import requests_cache
+    import lxml.html
+    from lxml.cssselect import CSSSelector
+
+    requests_cache.install_cache('cache')
+    resposta = requests.post(endereco, data=dados)
+    conteudo = lxml.html.fromstring(resposta.content)
+    return CSSSelector(seletor)(conteudo)
+
